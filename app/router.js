9480ea982b24da72abe9e59ed72aa660b8a3e243
Jay Varner <jayvarner@gmail.com>
2015-05-13 16:02:13 -0400

    Adding controllers, routes and models.

--- app/router.js
+++ app/router.js
@@ -6,4 +6,8 @@ var Router = Ember.Router.extend({
 });
 
 export default Router.map(function() {
+  this.route('edit-project-modal');
+  this.route('project');
+  this.route('about');
+  this.route('project-layer');
 });
---
+++ tests/unit/helpers/is-collaborator-test.js
@@ -0,0 +1,12 @@
+import {
+  isCollaborator
+} from '../../../helpers/is-collaborator';
+import { module, test } from 'qunit';
+
+module('IsCollaboratorHelper');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = isCollaborator(42);
+  assert.ok(result);
+});
