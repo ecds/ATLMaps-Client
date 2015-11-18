c2433c08c670f19b11adc75e55af092d29ef2adb
Alex Zotov <alexandr.zotov@emory.edu>
2015-11-18 09:54:46 -0500

    authentication error handling

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -2,5 +2,23 @@ import Ember from 'ember';
 import LoginControllerMixin from 'simple-auth/mixins/login-controller-mixin';
 
 export default Ember.Controller.extend(LoginControllerMixin, {
-  authenticator: 'simple-auth-authenticator:oauth2-password-grant'
+
+  authenticator: 'simple-auth-authenticator:oauth2-password-grant',
+  
+  actions: {
+
+    // display an error when authentication fails
+    authenticate: function () {
+      var self = this;
+      this._super().then(function () {
+        Ember.Logger.debug('Session authentication succeeded');
+      }, function (error) {
+        Ember.Logger.debug('Session authentication failed with message:',
+          error.message);
+        self.set('errorMessage', 'Invalid email/password combination.');
+      });
+    }
+
+  }
+
 });
