bdc12cc87cd35b70e357c76d2a6bd99e50a73918
Jay Varner <jayvarner@gmail.com>
2015-12-03 11:24:25 -0500

    Just added a few comments.

--- app/routes/projects.js
+++ app/routes/projects.js
@@ -9,11 +9,12 @@ export default Ember.Route.extend({
         didTransition: function() {
             Ember.$(document).attr('title', 'ATLMaps: Projects');
         },
-        
+
         createProject: function() {
-            
+
+			// Placehoder name for project.
             var newProjectName = md5((new Date()).toTimeString());
-            
+
             var project = this.store.createRecord('project', {
                 name: newProjectName,
                 user_id: this.session.get('content.secure.user.id'),
@@ -23,16 +24,17 @@ export default Ember.Route.extend({
                 zoom_level: 13,
                 default_base_map: 'street'
             });
-            
+
             var _this = this;
-            
+
             var onSuccess = function() {
-                
+
                 var newProject = DS.PromiseObject.create({
                     promise: _this.store.query('project', { name: newProjectName })
                 });
-    
+
                 newProject.then(function() {
+					// Set the name so we don't just have some ugly string in there.
                 	newProject.get('firstObject').set('name', 'Please enter a title.');
                     _this.transitionTo('project.edit', newProject.get('firstObject').id);
                 });
