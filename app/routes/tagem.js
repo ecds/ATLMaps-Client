c27216ba619db4a49f4cb1da95cd1d486f528f84
Jay Varner <jayvarner@gmail.com>
2016-04-29 14:33:26 -0400

    trying to fix tagem route on the QA server. maybe loading all the raster layers is too much.

--- app/routes/tagem.js
+++ app/routes/tagem.js
@@ -3,8 +3,8 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model(){
         return Ember.RSVP.hash({
-            rasters: this.store.findAll('raster-layer'),
-            vectors: this.store.findAll('vector-layer'),
+            // rasters: this.store.findAll('raster-layer'),
+            // vectors: this.store.findAll('vector-layer'),
             categories: this.store.findAll('category'),
         });
     },
