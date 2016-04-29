4c21ae90a446e27b290dff2252fc923727ec8108
Jay Varner <jayvarner@gmail.com>
2016-04-29 10:21:47 -0400

    fix for switching base layers.

--- app/routes/project/base-layers.js
+++ app/routes/project/base-layers.js
@@ -37,6 +37,7 @@ export default Ember.Route.extend({
         },
 
 		setBase: function(base, model, editing) {
+            console.log(base)
             Ember.$('.base').hide();
             Ember.$('.'+base).show();
 			Ember.$(".leaflet-control-layers-selector").removeAttr('checked');
