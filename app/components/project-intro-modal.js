41f3eb1ff15dcee1e2eea9a7f185ce8930ead8a0
Jay Varner <jayvarner@gmail.com>
2016-08-01 17:14:53 -0400

    Now use esc to close intro modal.

--- app/components/project-intro-modal.js
+++ app/components/project-intro-modal.js
@@ -1,23 +1,32 @@
 import Ember from 'ember';
+import { EKMixin, keyUp } from 'ember-keyboard';
 
-export default Ember.Component.extend({
+export default Ember.Component.extend(EKMixin,{
     showIntro: Ember.inject.service(),
     classNames: ['intro-modal-link'],
 
+    activateKeyboard: Ember.on('init', function() {
+        this.set('keyboardActivated', true);
+    }),
+
     didInsertElement: function(){
 
-        // Pick the layout from the attribute
-        if (this.get('project.templateSlug') === 'article-and-video'){
-			this.set('av', true);
-		}
-        else if (this.get('project.templateSlug') === 'article-only'){
-            this.set('a', true);
-        }
-        else if (this.get('project.templateSlug') === 'video-only'){
-            this.set('v', true);
-        }
+        // // Pick the layout from the attribute
+        // if (this.get('project.templateSlug') === 'article-and-video'){
+        //     this.set('av', true);
+        // }
+        // else if (this.get('project.templateSlug') === 'article-only'){
+        //     this.set('a', true);
+        // }
+        // else if (this.get('project.templateSlug') === 'video-only'){
+        //     this.set('v', true);
+        // }
 
     },
+
+    closeWithEsc: Ember.on(keyUp('Escape'), function() {
+        this.sendAction('action');
+    }),
 
     actions: {
 
