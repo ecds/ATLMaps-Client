6dd96e5e51497cfc6330798eaaca20a5444ddc8d
Jay Varner <jayvarner@gmail.com>
2016-11-25 01:37:42 -0500

    moved the 'remove layer' button and re-worded the spatial search link.

--- app/components/search-list-results.js
+++ app/components/search-list-results.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    classNames: ['col', 's12', 'scroll-container'],
+    classNames: ['scroll-container'],
 
     nextPage(meta) {
         this.getResults(meta.next_page);
