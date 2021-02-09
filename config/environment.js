7000dfdc8f16b29694232bef81b373758e941def
Jay Varner <jayvarner@gmail.com>
2021-02-09 18:32:31 -0500

    Checkin environment file

---
+++ config/environment.js
@@ -0,0 +1,102 @@
+'use strict';
+
+module.exports = function(environment) {
+  let ENV = {
+    modulePrefix: 'atlmaps-client',
+    environment,
+    rootURL: '/',
+    locationType: 'auto',
+    EmberENV: {
+      FEATURES: {
+        // Here you can enable experimental features on an ember canary build
+        // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
+      },
+      EXTEND_PROTOTYPES: {
+        // Prevent Ember Data from overriding Date.parse.
+        Date: false
+      }
+    },
+
+    APP: {
+      // Here you can pass flags/options to your application instance
+      // when it is created
+      CENTER_LAT: 33.75440100,
+      CENTER_LNG: -84.3898100,
+      INITIAL_ZOOM: 14,
+      DEFAULT_BASE_MAP: 'grayscale',
+      API_HOST: 'https://api.atlmaps-dev.com',
+      DEFAULT_IMAGE: '/assets/images/logo-med.jpg'
+    },
+
+    fastboot: {
+      hostWhitelist: ['api.atlmaps-dev.com', /^lvh.me:\d+$/, '192.168.1.22:4200']
+    },
+
+    torii: {
+      sessionServiceName: 'session',
+      providers: {
+        ecds: {}
+      }
+    },
+
+    fauxOAuth: {
+      baseUrl: 'http://auth.digitalscholarship.emory.edu/auth/'
+    }
+  };
+
+  if (environment === 'development') {
+    // ENV.APP.LOG_RESOLVER = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_TRANSITIONS = true;
+    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV['ember-cli-mirage'] = { enabled: false, autostart: false };
+    ENV.APP.API_HOST = 'https://api.atlmaps-dev.com:3000';
+    // ENV.APP.API_HOST = 'https://api.atlmaps.com';
+    ENV['fauxOAuth'].tokenValidationUrl = 'https://api.atlmaps-dev.com:3000/auth/verify/';
+    ENV['fauxOAuth'].tokenAuthUrl = 'https://api.atlmaps-dev.com:3000/auth/tokens/';
+    ENV['fauxOAuth'].redirectUrl = 'https://lvh.me:4200/torii/redirect.html';
+
+  }
+
+  if (environment === 'test') {
+    // Testem prefers this...
+    ENV.locationType = 'none';
+    ENV.APP.API_HOST = '';
+    ENV['fauxOAuth'].tokenAuthUrl = '/tokens/';
+    ENV['ember-cli-mirage'] = { enabled: true, autostart: true };
+    ENV['ember-a11y-testing'] = {
+      componentOptions: {
+        axeOptions: {
+          rules: {
+            'color-contrast': { enabled: false } // enable region rule
+          },
+          checks: {
+            'color-contrast': { options: { noScroll: true } } // disable scrolling of color-contrast check
+          }
+        }
+      }
+    };
+
+    // keep test console output quieter
+    ENV.APP.LOG_ACTIVE_GENERATION = false;
+    ENV.APP.LOG_VIEW_LOOKUPS = false;
+
+    ENV.APP.rootElement = '#ember-testing';
+    ENV.APP.autoboot = false;
+  }
+
+  if (environment === 'production') {
+    // here you can enable a production-specific feature
+  }
+
+  if (environment == 'staging') {
+    ENV.APP.API_HOST = 'https://atlmaps-api.ecdsdev.org';
+    // ENV.APP.API_HOST = 'https://api.atlmaps.com';
+    ENV['fauxOAuth'].tokenValidationUrl = 'https://atlmaps-api.ecdsdev.org/auth/verify/';
+    ENV['fauxOAuth'].tokenAuthUrl = 'https://atlmaps-api.ecdsdev.org/auth/tokens/';
+    ENV['fauxOAuth'].redirectUrl = 'https://atlmaps.ecdsdev.org/torii/redirect.html';
+  }
+
+  return ENV;
+};
